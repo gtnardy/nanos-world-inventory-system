f9ff9b8dd6467e102a93a116f0d7ce1f75cd1546
Gabriel T. Nardy <gtnardy@gmail.com>
2021-07-11 16:42:56 -0300

    updated to the newer api version

--- Client/Index.lua
+++ Client/Index.lua
@@ -22,7 +22,7 @@ InventoryKeyBinding = {
 }
 
 -- When package loads, verify if LocalPlayer already exists (eg. when reloading the package), then try to get and store it's controlled character
-Package:Subscribe("Load", function()
+Package.Subscribe("Load", function()
 	-- Creates a WebUI for the Inventory
 	WeaponHUD = WebUI("InventoryHUD", "file:///UI/index.html")
 
@@ -32,12 +32,12 @@ Package:Subscribe("Load", function()
 	end)
 end)
 
-Package:Subscribe("Unload", function()
+Package.Subscribe("Unload", function()
 	WeaponHUD:Destroy()
 end)
 
 -- Catches KeyUp event to see if it was pressed any Inventory Shortcut key
-Client:Subscribe("KeyUp", function(KeyName)
+Client.Subscribe("KeyUp", function(KeyName)
 	local slot = -1
 
 	if (InventoryKeyBinding[KeyName]) then
@@ -46,51 +46,51 @@ Client:Subscribe("KeyUp", function(KeyName)
 
 	-- If pressed any shortcut key
 	if (slot ~= -1) then
-		local inventory = NanosWorld:GetLocalPlayer():GetValue("Inventory") or {}
+		local inventory = Client.GetLocalPlayer():GetValue("Inventory") or {}
 
 		-- Verifies if I have any item in the index of that inventory or if I pressed 0 (means remove all items from hand), then send 'SwitchInventoryItem' to server to switch my current item
 		if (inventory[slot] or slot == 0) then
-			Events:CallRemote("SwitchInventoryItem", {slot})
+			Events.CallRemote("SwitchInventoryItem", slot)
 		end
 	end
 end)
 
-Events:Subscribe("SwitchedInventoryItem", function(slot)
-	WeaponHUD:CallEvent("SwitchedInventoryItem", {slot})
+Events.Subscribe("SwitchedInventoryItem", function(slot)
+	WeaponHUD:CallEvent("SwitchedInventoryItem", slot)
 end)
 
--- When LocalPlayer spawns, sets an event on it to trigger when we possesses a new character, to store the local controlled character locally. This event is only called once, see Package:Subscribe("Load") to load it when reloading a package
-NanosWorld:Subscribe("SpawnLocalPlayer", function(local_player)
+-- When LocalPlayer spawns, sets an event on it to trigger when we possesses a new character, to store the local controlled character locally. This event is only called once, see Package.Subscribe("Load") to load it when reloading a package
+NanosWorld.Subscribe("SpawnLocalPlayer", function(local_player)
 	IsLocalPlayerReady = true
 	SetupUI()
 end)
 
 -- Receives a new item on the inventory
-Events:Subscribe("GiveInventoryItem", function(inventory_item_id)
+Events.Subscribe("GiveInventoryItem", function(inventory_item_id)
 	-- Gets if the item exists item from InventoryItems list
 	local InventoryItem = InventoryItems[inventory_item_id]
 
 	-- Gets my inventory, sets the item at the slot and saves it again
-	local inventory = NanosWorld:GetLocalPlayer():GetValue("Inventory") or {}
+	local inventory = Client.GetLocalPlayer():GetValue("Inventory") or {}
 	inventory[InventoryItem.slot] = {id = inventory_item_id}
-	NanosWorld:GetLocalPlayer():SetValue("Inventory", inventory)
+	Client.GetLocalPlayer():SetValue("Inventory", inventory)
 
 	-- Calls HUD to add this item to the screen
 	if (WeaponHUD) then
-		WeaponHUD:CallEvent("AddInventoryItem", {InventoryItem.slot, InventoryItem.name, InventoryItem.image})
+		WeaponHUD:CallEvent("AddInventoryItem", InventoryItem.slot, InventoryItem.name, InventoryItem.image)
 	end
 end)
 
 -- Removes the item from inventory (called from server)
 Events:Subscribe("RemoveInventoryItem", function(slot)
 	-- Gets my inventory, sets the item at slot to nil and saves it again
-	local inventory = NanosWorld:GetLocalPlayer():GetValue("Inventory") or {}
+	local inventory = Client.GetLocalPlayer():GetValue("Inventory") or {}
 	inventory[slot] = nil
-	NanosWorld:GetLocalPlayer():SetValue("Inventory", inventory)
+	Client.GetLocalPlayer():SetValue("Inventory", inventory)
 
 	-- Calls HUD to remove it from screen
 	if (WeaponHUD) then
-		WeaponHUD:CallEvent("RemoveInventoryItem", {slot})
+		WeaponHUD:CallEvent("RemoveInventoryItem", slot)
 	end
 end)
 
@@ -102,9 +102,9 @@ function SetupUI()
 	local inventory = NanosWorld:GetLocalPlayer():GetValue("Inventory")
 	if (inventory) then
 		for slot, data in pairs(inventory) do
-			WeaponHUD:CallEvent("AddInventoryItem", {slot, InventoryItems[data.id].name, InventoryItems[data.id].image})
+			WeaponHUD:CallEvent("AddInventoryItem", slot, InventoryItems[data.id].name, InventoryItems[data.id].image)
 		end
 	end
 
-	Events:CallRemote("RemotePlayerReady", {})
+	Events.CallRemote("RemotePlayerReady")
 end
